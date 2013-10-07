90aff3f530e9ead6a82a99b35d375c288b674650
jkubrynski <jkubrynski@gmail.com>
2013-10-07 22:19:16 +0200

    First API version

--- src/main/java/eu/codearte/fairyland/Hook.java
+++ src/main/java/eu/codearte/fairyland/Hook.java
@@ -1,11 +1,34 @@
 package eu.codearte.fairyland;
 
+import eu.codearte.fairyland.producer.HookProducer;
+
+import java.util.Locale;
+
 /**
  * Entry class
- * @author jkubrynski@gmail.com
+ * @author Codearte
  * @since 2013-10-07
  */
 public class Hook {
 
+  private Hook(Locale locale) {
+
+  }
+
+  public static Hook director() {
+    return director(Locale.ENGLISH);
+  }
+
+  public static Hook director(Locale locale) {
+    return new Hook(locale);
+  }
+
+  public <T extends HookProducer> T produce(Class<T> producer) {
+    try {
+      return producer.newInstance();
+    } catch (InstantiationException | IllegalAccessException e) {
+      throw new IllegalArgumentException(e);
+    }
+  }
 
 }
---
+++ src/main/java/eu/codearte/fairyland/producer/HookProducer.java
@@ -0,0 +1,9 @@
+package eu.codearte.fairyland.producer;
+
+/**
+ * @author jkubrynski@gmail.com
+ * @since 2013-10-07
+ */
+public abstract class HookProducer {
+
+}
