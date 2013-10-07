042adc86c31b27823e5a63c358fec7c1294a39e4
Mariusz Smykuła <mariuszs@gmail.com>
2013-10-07 23:01:13 +0200

    takie cos

--- src/test/groovy/eu/codearte/fairyland/Sample.groovy
+++ src/test/groovy/eu/codearte/fairyland/Sample.groovy
@@ -2,4 +2,15 @@ package eu.codearte.fairyland
 
 import static eu.codearte.fairyland.Hook.director
 
+// 1.
+
 println director().person().firstName()
+// Alice
+println director().person().firstName()
+// Bob
+
+// 2
+println director().person().fistName()
+//Alice
+println director().person().firstName()
+//Alice surname
