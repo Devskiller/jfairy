e5ed704d3b99c02449918f0846c5f375460dda8f
jkubrynski <jkubrynski@gmail.com>
2013-11-15 19:27:45 +0100

    First stage of the DI introduction

--- src/main/java/eu/codearte/fairyland/Fairy.java
+++ src/main/java/eu/codearte/fairyland/Fairy.java
@@ -4,114 +4,137 @@
 package eu.codearte.fairyland;
 
 import eu.codearte.fairyland.producer.Company;
-import eu.codearte.fairyland.producer.util.RandomGenerator;
+import eu.codearte.fairyland.producer.person.NationalIdentificationNumber;
+import eu.codearte.fairyland.producer.person.NationalIdentityCardNumber;
 import eu.codearte.fairyland.producer.person.Person;
 import eu.codearte.fairyland.producer.person.Sex;
+import eu.codearte.fairyland.producer.person.pl.NIP;
+import eu.codearte.fairyland.producer.person.pl.Pesel;
 import eu.codearte.fairyland.producer.person.pl.PolishIdentityCardNumber;
+import eu.codearte.fairyland.producer.person.pl.VATIdentificationNumber;
 import eu.codearte.fairyland.producer.text.FairUtil;
 import eu.codearte.fairyland.producer.text.Text;
+import eu.codearte.fairyland.producer.text.TextProducer;
 import eu.codearte.fairyland.producer.util.DateGenerator;
+import eu.codearte.fairyland.producer.util.RandomDataGenerator;
+import eu.codearte.fairyland.producer.util.RandomGenerator;
 import eu.codearte.fairyland.producer.util.TimeProvider;
 import org.joda.time.DateTime;
+import org.picocontainer.Characteristics;
+import org.picocontainer.MutablePicoContainer;
+import org.picocontainer.PicoBuilder;
 
 import java.io.IOException;
 import java.util.Locale;
-
-import static eu.codearte.fairyland.FairyFactory.*;
 
 /**
  * Entry class
  */
 public final class Fairy {
 
-   private static final String DATA_FILE_PREFIX = "fairyland_";
-   private static final int SEED = 1761283695;
+  private static final String DATA_FILE_PREFIX = "fairyland_";
+  private static final int SEED = 1761283695;
+  private final MutablePicoContainer pico;
 
-   private DataMaster dataMaster;
-   private final RandomGenerator randomGenerator = new RandomGenerator(SEED);
-   private final TimeProvider timeProvider = new TimeProvider();
-   private final FairUtil fairUtil = createStringifyUtil(randomGenerator, timeProvider);
-   private final DateGenerator dateGenerator = new DateGenerator(randomGenerator, timeProvider);
+  private DataMaster dataMaster;
+  private final RandomGenerator randomGenerator = new RandomGenerator(SEED);
+  private final TimeProvider timeProvider = new TimeProvider();
+  private final DateGenerator dateGenerator = new DateGenerator(randomGenerator, timeProvider);
 
-   private Fairy(Locale locale, String filePrefix) {
+  private Fairy(Locale locale, String filePrefix) {
+    pico = new PicoBuilder().withCaching().withConstructorInjection().build();
 
-      try {
-         dataMaster = new DataMaster();
-         dataMaster.readResources(filePrefix + locale.getLanguage() + ".yml");
-      } catch (IOException e) {
-         throw new IllegalStateException(e);
-      }
-   }
+    try {
+      dataMaster = new DataMaster();
+      dataMaster.readResources(filePrefix + locale.getLanguage() + ".yml");
+    } catch (IOException e) {
+      throw new IllegalStateException(e);
+    }
 
-   /**
-    * Use this factory method to create dataset containing default fairyland_{langCode}.yml files
-    * merged with custom files with the same name
-    *
-    * @return Fairy instance
-    */
-   public static Fairy create() {
-      return create(Locale.ENGLISH);
-   }
+    pico.as(Characteristics.CACHE).addComponent(dataMaster);
+    pico.addComponent(randomGenerator);
+    pico.addComponent(timeProvider);
+    pico.addComponent(FairUtil.class);
+    pico.addComponent(dateGenerator);
+    pico.addComponent(RandomDataGenerator.class);
+    pico.addComponent(Text.class);
+    pico.addComponent(TextProducer.class);
+    pico.addComponent(Person.class).as(Characteristics.CACHE);
+    pico.addComponent(NationalIdentificationNumber.class, Pesel.class);
+    pico.addComponent(NationalIdentityCardNumber.class, PolishIdentityCardNumber.class);
+    pico.addComponent(VATIdentificationNumber.class, NIP.class);
+    pico.as(Characteristics.NO_CACHE).addComponent(Company.class);
+  }
 
-   /**
-    * Use this factory method to create dataset containing default fairyland_{langCode}.yml files
-    * merged with custom files with the same name
-    *
-    * @param locale will be used to assess langCode for data file
-    * @return Fairy instance
-    */
-   public static Fairy create(Locale locale) {
-      return create(locale, DATA_FILE_PREFIX);
-   }
+  /**
+   * Use this factory method to create dataset containing default fairyland_{langCode}.yml files
+   * merged with custom files with the same name
+   *
+   * @return Fairy instance
+   */
+  public static Fairy create() {
+    return create(Locale.ENGLISH);
+  }
 
-   /**
-    * Use this factory method to create your own dataset overriding bundled one
-    *
-    * @param locale         will be used to assess langCode for data file
-    * @param dataFilePrefix prefix of the data file - final pattern will be dataFilePrefix_{langCode}.yml
-    * @return Fairy instance
-    */
-   public static Fairy create(Locale locale, String dataFilePrefix) {
-      return new Fairy(locale, dataFilePrefix);
-   }
+  /**
+   * Use this factory method to create dataset containing default fairyland_{langCode}.yml files
+   * merged with custom files with the same name
+   *
+   * @param locale will be used to assess langCode for data file
+   * @return Fairy instance
+   */
+  public static Fairy create(Locale locale) {
+    return create(locale, DATA_FILE_PREFIX);
+  }
+
+  /**
+   * Use this factory method to create your own dataset overriding bundled one
+   *
+   * @param locale         will be used to assess langCode for data file
+   * @param dataFilePrefix prefix of the data file - final pattern will be dataFilePrefix_{langCode}.yml
+   * @return Fairy instance
+   */
+  public static Fairy create(Locale locale, String dataFilePrefix) {
+    return new Fairy(locale, dataFilePrefix);
+  }
 
 
-   public Text text() {
-      return createText(dataMaster, randomGenerator, dateGenerator);
-   }
+  public Text text() {
+    return pico.getComponent(Text.class);
+  }
 
-   public Person person() {
-      return createPerson(dataMaster, randomGenerator, fairUtil, dateGenerator);
-   }
+  public Person person() {
+    return pico.getComponent(Person.class);
+  }
 
-   public Company company() {
-      return createCompany(dataMaster, randomGenerator, dateGenerator);
-   }
+  public Company company() {
+    return pico.getComponent(Company.class);
+  }
 
-   public String nationalIdentityNumber() {
-      return new PolishIdentityCardNumber(randomGenerator).generate(
-            dateGenerator.randomDateBetweenYears(2000, timeProvider.getCurrentYear()));
-   }
+  public String nationalIdentityNumber() {
+    return pico.getComponent(NationalIdentityCardNumber.class).generate(
+        dateGenerator.randomDateBetweenYears(2000, timeProvider.getCurrentYear()));
+  }
 
-   public String nationalIdentificationNumber() {
-      return createNationalIdentificationNumber(randomGenerator)
-            .generate(dateGenerator.randomDateInThePast(10),
-                  randomGenerator.trueOrFalse() ? Sex.male : Sex.female);
-   }
+  public String nationalIdentificationNumber() {
+    return pico.getComponent(NationalIdentificationNumber.class)
+        .generate(dateGenerator.randomDateInThePast(10),
+            randomGenerator.trueOrFalse() ? Sex.male : Sex.female);
+  }
 
-   public String numerify(String numberString) {
-      return fairUtil.numerify(numberString);
-   }
+  public String numerify(String numberString) {
+    return pico.getComponent(FairUtil.class).numerify(numberString);
+  }
 
-   public String letterify(String letterString) {
-      return fairUtil.letterify(letterString);
-   }
+  public String letterify(String letterString) {
+    return pico.getComponent(FairUtil.class).letterify(letterString);
+  }
 
-   public String bothify(String string) {
-      return fairUtil.bothify(string);
-   }
+  public String bothify(String string) {
+    return pico.getComponent(FairUtil.class).bothify(string);
+  }
 
-   public DateTime randomDateInThePast() {
-      return dateGenerator.randomDateInThePast(100);
-   }
+  public DateTime randomDateInThePast() {
+    return dateGenerator.randomDateInThePast(100);
+  }
 }
