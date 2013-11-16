b8c3f529f877dfc79a2cf761f71cc63140e5d999
Mariusz Smykula <mariuszs@gmail.com>
2013-11-16 21:43:11 +0100

    better test

--- src/test/groovy/eu/codearte/fairyland/producer/person/pl/AlphaNumberSystemSpec.groovy
+++ src/test/groovy/eu/codearte/fairyland/producer/person/pl/AlphaNumberSystemSpec.groovy
@@ -17,7 +17,7 @@ class AlphaNumberSystemSpec extends Specification {
 
 	@Unroll void "Number #x should be converted to #y"() {
 		expect:
-		convertToString(x, 26) == y;
+		convertToString(x, ('A' .. 'Z').size()) == y;
 		where:
 		x  | y
 		0  | "A"
