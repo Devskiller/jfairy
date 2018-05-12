525a5d0a0729a68e7167b0e31ff4e20cb0b7599e
Olek Kamczyc <aleksander.kamczyc@gmail.com>
2018-05-12 13:34:11 +0200

    Add information to Country about Currency

--- src/main/java/com/devskiller/jfairy/producer/person/Country.java
+++ src/main/java/com/devskiller/jfairy/producer/person/Country.java
@@ -4,29 +4,33 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.devskiller.jfairy.producer.util.CurrencyCode;
 import com.devskiller.jfairy.producer.util.LanguageCode;
 
 public enum Country {
-	Poland("PL", LanguageCode.PL),
-	UnitedKingdom("GB", LanguageCode.EN),
-	Australia("AU", LanguageCode.EN),
-	USA("US", LanguageCode.EN),
-	Canada("CA", LanguageCode.EN, LanguageCode.FR),
-	Spain("ES", LanguageCode.ES),
-	France("FR", LanguageCode.FR),
-	Georgia("GE", LanguageCode.KA),
-	Italy("IT", LanguageCode.IT),
-	Germany("DE", LanguageCode.DE),
-	Sweden("SE", LanguageCode.SV),
-	China("CN", LanguageCode.ZH);
+	Poland("PL", CurrencyCode.PLN, LanguageCode.PL),
+	UnitedKingdom("GB", CurrencyCode.GBP, LanguageCode.EN),
+	Australia("AU", CurrencyCode.AUD, LanguageCode.EN),
+	USA("US", CurrencyCode.USD, LanguageCode.EN),
+	Canada("CA", CurrencyCode.CAD, LanguageCode.EN, LanguageCode.FR),
+	Spain("ES", CurrencyCode.EUR, LanguageCode.ES),
+	France("FR", CurrencyCode.EUR, LanguageCode.FR),
+	Georgia("GE", CurrencyCode.GEL, LanguageCode.KA),
+	Italy("IT", CurrencyCode.EUR, LanguageCode.IT),
+	Germany("DE", CurrencyCode.EUR, LanguageCode.DE),
+	Sweden("SE", CurrencyCode.SEK, LanguageCode.SV),
+	China("CN", CurrencyCode.RMB, LanguageCode.ZH);
 
 	//	ISO 3166 code
 	private final String code;
-	// ISO 639-1
+	//	ISO 639-1
 	private final List<LanguageCode> languages;
+	//	ISO 4217
+	private final CurrencyCode currencyCode;
 
-	Country(String code, LanguageCode... language) {
+	Country(String code, CurrencyCode currencyCode, LanguageCode... language) {
 		this.code = code;
+		this.currencyCode = currencyCode;
 		this.languages = Arrays.asList(language);
 	}
 
@@ -38,5 +42,9 @@ public enum Country {
 		return Arrays.stream(Country.values())
 			.filter(country -> country.languages.contains(language))
 			.collect(Collectors.toList());
+	}
+
+	public String getCurrencyCode() {
+		return currencyCode.name();
 	}
 }
